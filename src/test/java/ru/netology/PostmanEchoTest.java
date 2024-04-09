97eb1eb75d3613abf70f5cdc5498ca620af1ad42
Alex Zakharov <zakharov61@yandex.ru>
2024-04-09 18:28:35 +0300

    post

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -13,13 +13,13 @@ class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-        // Выполняемые действия
+                // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
